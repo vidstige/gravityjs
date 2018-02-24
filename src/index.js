6e52b4a79dede41d7113885c84ad25914f0b9781
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-24 23:16:42 +0100

    acceleration from gravity

--- src/index.js
+++ src/index.js
@@ -19,11 +19,42 @@ function randomPoint() {
     return {x: Math.random() - 0.5, y: Math.random() - 0.5};
 }
 
+function diff(a, b) {
+    return {x: a.x - b.x, y: a.y - b.y};
+}
+function norm2(v) {
+    return v.x*v.x + v.y*v.y;
+}
+
+function gravity(a, b) {
+    const d = diff(a.p, b.p);
+    const r2 = norm2(d);
+    return {
+        x: d.x / r2,
+        y: d.y / r2
+    };
+}
+
 function step(stars, dt) {
+    var f = []; // force vectors
+
+    for (var i = 0; i < stars.length; i++) {
+        f[i] = {x: 0, y: 0};
+        for (var j = 0; j < stars.length; j++) {
+            if (i != j) {
+                const g = gravity(stars[i], stars[j]);
+                f[i].x += g.x;
+                f[i].y += g.y;
+            }
+        }   
+    }
+
     for (var i = 0; i < stars.length; i++) {
         const star = stars[i];
         star.p.x += 0.0001 * star.v.x * dt;
         star.p.y += 0.0001 * star.v.y * dt;
+        star.v.x += 0.0001 * f[i].x;
+        star.v.y += 0.0001 * f[i].y;
     }
 }
 
